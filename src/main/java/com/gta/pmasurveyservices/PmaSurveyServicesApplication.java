4dd2b4b3ba6550b7696aad8f956449ed1d4787aa
Chris Ivey <chrisivey01@gmail.com>
2019-11-08 13:00:19 -0600

    set up git ignore and survey services

---
+++ src/main/java/com/gta/pmasurveyservices/PmaSurveyServicesApplication.java
@@ -0,0 +1,13 @@
+package com.gta.pmasurveyservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmaSurveyServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmaSurveyServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gta/pmasurveyservices/PmaSurveyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gta.pmasurveyservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmaSurveyServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
